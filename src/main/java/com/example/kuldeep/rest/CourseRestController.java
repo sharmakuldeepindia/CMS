7edb363bd8f3d7938a3c96bc01780e70e10bac3e
sharmakuldeep1 <kuldeepkasampriya1@gmail.com>
2018-12-28 18:05:03 +0530

    login page style

--- src/main/java/com/example/kuldeep/rest/CourseRestController.java
+++ src/main/java/com/example/kuldeep/rest/CourseRestController.java
@@ -1,20 +1,22 @@
 package com.example.kuldeep.rest;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.model.Course;
-import com.example.model.User;
+import com.example.model.CourseFiles;
+import com.example.repository.CourseFilesRepository;
 import com.example.repository.CourseRepository;
 import com.example.service.UserService;
 
@@ -23,13 +25,15 @@ import com.example.service.UserService;
 
 public class CourseRestController {
 
-	@Autowired
-	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	
 	public static final Logger logger = LoggerFactory.getLogger(UserRestController.class);
 	@Autowired
 	UserService userService;
 	@Autowired
 	CourseRepository courseRepo;
+	@Autowired
+	CourseFilesRepository courseFilesRepo;
 	@RequestMapping(value = "/course", method = RequestMethod.GET)
 	public ResponseEntity<List<Course>> listAllCourses() {
 		List<Course> courses = courseRepo.findAll();
@@ -47,7 +51,11 @@ public class CourseRestController {
 			logger.error("Course with id {} not found.", id);
 			return new ResponseEntity(new String("Course with id " + id + " not found"), HttpStatus.NOT_FOUND);
 		}
-		return new ResponseEntity<Course>(course, HttpStatus.OK);
+		List<CourseFiles> cf = courseFilesRepo.findByCourseId(course.getId());
+		Map<String, Object> result = new HashMap<String,Object>();
+		    result.put("course",course);
+		    result.put("courseFiles",cf);
+		return new ResponseEntity<Map<String,Object>>(result, HttpStatus.OK);
 	}
 
 }
